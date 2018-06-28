94fbca9fe2bed0159d078b7bbae4cbf51d91c2a4
Jared Rickert <jaredrickert52@gmail.com>
2018-06-28 15:36:10 -0500

    Squashed commit of the following:
    commit bd1a3e1218125c260c1c14d1d4ec591851cda3b0
    Author: Alan Luo <i@iltc.io>
    Date:   Wed Jun 27 22:23:51 2018 -0500
    
        Remove password from user information
    
    commit 53567479a695bde12800c5f90facb4ecf965c827
    Author: Alan Luo <i@iltc.io>
    Date:   Wed Jun 27 22:18:23 2018 -0500
    
        Finished token verify and example
    
    commit 52b78669670c84acbd812439e9ed981e11e07ca9
    Author: Alan Luo <i@iltc.io>
    Date:   Wed Jun 27 14:56:03 2018 -0500
    
        Added code for jwt
    
    commit 66d18fee89ef01da2a03b4c71ac14639416953e7
    Author: Alan Luo <i@iltc.io>
    Date:   Wed Jun 27 14:54:40 2018 -0500
    
        Ignore some files for Git
    
    commit a44bfa33be15a581b61ffb82db0feada0476239e
    Author: Alan Luo <i@iltc.io>
    Date:   Wed Jun 27 14:52:55 2018 -0500
    
        Added lodash
    
    commit 06058f88a7bc4a543bcf7fa94e2de91aa519a129
    Author: Alan Luo <i@iltc.io>
    Date:   Tue Jun 26 16:04:17 2018 -0500
    
        Added jsonwebtoken
    
    commit 01d26cedd3ffbb8806b394ef5209d0e456baa4b2
    Author: Alan Luo <i@iltc.io>
    Date:   Tue Jun 26 16:03:28 2018 -0500
    
        Changed (almost) everything to TypeScript
    
    commit 19b3db3e7cbad2669a67803d2e156df9e33c5435
    Author: Alan Luo <i@iltc.io>
    Date:   Tue Jun 26 15:38:09 2018 -0500
    
        Converted to TypeScript
    
    commit 64b3312db667fd98ab297bf1da89bce1e46a9023
    Author: Alan Luo <i@iltc.io>
    Date:   Tue Jun 26 15:37:31 2018 -0500
    
        Converted to TypeScript
    
    commit 2b4eb53e18d39ae369f48dadbd4151e1767269c3
    Author: Alan Luo <iltc@wireless-nat-inside.uiowa.edu>
    Date:   Tue Jun 26 15:34:52 2018 -0500
    
        Added Types
    
    commit 689a96fe5618a3280ed318dcd67cc1d4b8a6d5f2
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 21:37:47 2018 -0500
    
        Changed code to support MongoDB 3.0+
    
    commit 7b6ec128fb0069b925763450f9f0ca6eb6822449
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 21:36:52 2018 -0500
    
        Added search or add user
    
    commit c865123bff6b5c5a3e45c854aaebbb473530b082
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 20:35:13 2018 -0500
    
        Added code to support DB connection and DB state
    
    commit 75ad660e674bdd9e83e0c7789554c35dd70f5061
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 20:12:42 2018 -0500
    
        Added code for user login
    
    commit 54f8936d2474c3a80e1d1223c1ea108099c2d8ba
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 20:10:38 2018 -0500
    
        Added Passport and User Router
    
    commit e6bf3524f3b933acac28cefadf04f9dcf5f692f6
    Author: iLtc <i@iltc.io>
    Date:   Sat Jun 23 20:09:29 2018 -0500
    
        Added Passport and MongoDB

--- server/config.ts
+++ server/config.ts
@@ -7,7 +7,24 @@ const resolveApp = relativePath => Path.resolve(appDirectory, relativePath);
 
 const env = process.env;
 export const isProduction = env.NODE_ENV === "production";
+export const isTest: boolean = env.NODE_ENV === "test";
 export const port = env.PORT || 3001;
 export const baseRoute = "/api";
 export const staticFiles = resolveApp("build");
 export const indexFile = resolveApp("build/index.html");
+
+const getSecretKey = key => {
+  if (isProduction && !key) {
+    if (!key) {
+      throw new Error("Secret key needs to be set for production");
+    } else {
+      return key;
+    }
+  } else if (isTest) {
+    return "test secret key";
+  } else {
+    return "develop secret key";
+  }
+};
+
+export const secretKey: string = getSecretKey(process.env.SECRET_KEY);
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import * as Express from "express";
 import * as bodyParser from "body-parser";
+import * as passport from "passport";
 
 import * as Config from "./config";
+import * as db from "./db";
+import { userRouter } from "./user";
 
 const app = Express();
 app.use(bodyParser.urlencoded({ extended: true })); // allow data from a post
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const router = Express.Router();
 
@@ -17,6 +23,7 @@ router.use((req, res) => {
   res.send("404: Page not Found");
 });
 
+app.use(`${Config.baseRoute}/user`, userRouter);
 app.use(Config.baseRoute, router);
 
 if (Config.isProduction) {
@@ -25,6 +32,16 @@ if (Config.isProduction) {
   app.use("/:drawingId", Express.static(Config.indexFile));
 }
 
-app.listen(Config.port, () => {
-  console.log(`Listening on port ${Config.port}`);
-});
+// Connect to Mongo on start
+// TODO: Move the url to config.ts
+db
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    app.listen(Config.port, () => {
+      console.log(`Listening on port ${Config.port}`);
+    });
+  })
+  .catch(err => {
+    console.log("Unable to connect to Mongo.");
+    process.exit(1);
+  });
